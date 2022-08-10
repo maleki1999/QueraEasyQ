83c5de674339f3ccb8ba27c32991af1912e53158
maleki1999 <m.maleki13781@gmail.com>
2022-08-09 17:06:36 -0700

    modified

--- 9_5/test/main.py
+++ 9_5/test/main.py
@@ -1,23 +1,20 @@
-def check_answer(correct , answer_sheet):
-    if '#' not in answer_sheet:
-        return 'N'
-    elif answer_sheet.count('#')==1 and answer_sheet.index('#') == correct-1:
-        return 'T'
-    else : return 'F'
-    
-contents = [input() for _ in range(3)]
-dic = {
+def check_answer(correct , n_q):
+    status = []
+    dic = {
     "A" : 1,
     "B" : 2,
     "C" : 3,
     "D" : 4
-}
-status = []
+    }
+    
+    for i in range(n_q):
+        answer_sheet = input()
+        if '#' in answer_sheet:   
+            status.append('T' if answer_sheet.count('#')==1 and answer_sheet.index('#') == dic[correct[i]]-1 else 'F')
+    return 3*status.count('T')-status.count('F')
+
+n_q , a , n_sheet = [input() for _ in range(3)]
 grades = []
-for i in range(int(contents[2])):
-    for j in range(int(contents[0])):
-        status.append(check_answer(dic[contents[1][j]],input())) 
-        
-    grades.append(3*status.count('T')-status.count('F'))
-    status = []
+for _ in range(int(n_sheet)): 
+    grades.append(check_answer(a ,int(n_q)))
 print(*grades , sep='\n')
